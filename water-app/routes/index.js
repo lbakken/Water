66991a1856cfdc02daca346fb6201015966b006d
Luke Bakken <lukecbakken@gmail.com>
2019-02-26 15:30:00 -0700

    added necessary jade files and began update on routes

--- water-app/routes/index.js
+++ water-app/routes/index.js
@@ -3,7 +3,17 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', { title: 'Bonsai Buddy' });
 });
+
+/* GET login page. */
+router.get('/login', function(req, res, next) {
+  res.render('login', {});
+})
+
+/* GET register page. */
+router.get('/register', function(req, res, next) {
+  res.render('register', {});
+})
 
 module.exports = router;
