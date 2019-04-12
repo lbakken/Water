5514baa6bbe567d28f24443627e4a3934e08c8d2
Luke Bakken <lukecbakken@gmail.com>
2019-04-12 11:45:37 -0600

    More initial updates, still not functional

--- auth.js
+++ auth.js
@@ -13,11 +13,13 @@ var knex = require('knex')({
 })
 
 passport.serializeUser(function(user, done) {
+    alert('called serializeUser')
     done(null, user.id);
 });
   
 passport.deserializeUser(function(id, done) {
-    knex.select().from('users').where({ id }).timeout(1000, {cancel: true}).then((user) => {
+    alert('called deserializeUser')
+    knex.select().from('users').where({ id }).timeout(3000, {cancel: true}).then((user) => {
         if (err) { return done(err) }
         else { done(null, user) }
     });
@@ -25,15 +27,57 @@ passport.deserializeUser(function(id, done) {
 
 passport.use(new Strategy(
     function(username, password, done) {
-      knex.select().from('users').where({ username }).timeout(1000, {cancel: true}).then((user) => {
-        if (err) { return done(err) }
+      knex.select().from('users').where({ username }).timeout(3000, {cancel: true}).then((user) => {
+        if (err) { 
+            alert('error on request')
+            return done(err) }
         else if (!user) {
+            alert('user not found')
             return done(null, false, { message: 'Incorrect username.' })
         }
         else if (!bcrypt.compareSync(password, user.password)) {
+            alert('password incorrect')
             return done(null, false, { message: 'Incorrect password.' })
         }
-        else { return done(null, user, { message: 'Successful login.' }) }
+        else { 
+            alert('success!')
+            return done(null, user, { message: 'Successful login.' }) }
       })
     }
 ));
+
+function isOpenUsername(username) {
+    alert('called isOpenUsername')
+    knex.select().from('users').where({ username }).timeout(3000, {cancel: true}).then((user) => {
+        if (err) { return done(err) }
+        else if (user) { return false }
+        else { return true }
+    })
+}
+
+function createUser(req) {
+    alert('called createUser')
+    var salt = bcrypt.genSaltSync();
+    var hash = bcrypt.hashSync(req.body.password, salt);
+    var create = new Date();
+    return knex('users').insert({
+        username: req.body.username,
+        password_salt: salt,
+        password_hash: hash,
+        email: req.body.email,
+        created: create
+    }).returning('*');
+}
+
+function loginRequired(req, res, next) {
+    alert('called loginRequired')
+    if (!req.user) return res.status(401).json({status: 'Please log in'});
+    return next();
+}
+
+module.exports = {
+    passport,
+    createUser,
+    loginRequired,
+    isOpenUsername
+}
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,8 @@
 var express = require('express');
 var router = express.Router();
-
+var passport = require('passport');
+var auth = require('../auth')
+passport.use(auth)
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bonsai Buddy' });
@@ -11,16 +13,19 @@ router.get('/login', function(req, res, next) {
   res.render('login', {});
 })
 
-router.post('/login', function(req, res, next) {
-  passport.authenticate('local', function(err, user, info) {
-    if (err) { return next(err); }
-    if (!user) { return res.redirect('/login'); }
-    req.logIn(user, function(err) {
-      if (err) { return next(err); }
-      return res.redirect('/users/' + user.username);
-    });
-  })(req, res, next);
-});
+router.post('/login', passport.authenticate('local', { successRedirect: '/',
+                                 failureRedirect: '/login',
+                                 failureFlash: true })
+);
+  // passport.authenticate('local', function(err, user, info) {
+  //   if (err) { return next(err); }
+  //   if (!user) { return res.redirect('/login'); }
+  //   req.logIn(user, function(err) {
+  //     if (err) { return next(err); }
+  //     else { return res.redirect('/users/' + user.username) }
+  //   });
+  // })(req, res, next);
+// });
 
 router.get('/logout', function(req, res, next) {
   req.logout()
@@ -31,6 +36,19 @@ router.get('/logout', function(req, res, next) {
 router.get('/register', function(req, res, next) {
   res.render('register', {});
 })
+
+// router.post('/register', function(req, res, next) {
+//   return passport.createUser(req, res).then((response) => {
+//     passport.authenticate('local', function(err, user, info) {
+//       if (err) { return next(err) }
+//       if (!user) { return res.redirect('/login') }
+//       req.login(user, function(err) {
+//         if (err) { return next(err) }
+//         else { return res.redirect('/users/' + user.username) }
+//       })
+//     })
+//   })
+// })
 
 /* GET userHome page. */
 router.get('/userHome', function(req, res, next) {
