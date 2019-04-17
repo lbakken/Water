df85a950ae2fcd7f9bb2d115f482637e6c101536
Luke Bakken <lukecbakken@gmail.com>
2019-04-17 12:21:40 -0600

    misc commit

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,8 @@
 var express = require('express');
 var router = express.Router();
 var passport = require('passport');
-var auth = require('../auth')
-passport.use(auth)
+// var passport = require('../auth')
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Bonsai Buddy' });
