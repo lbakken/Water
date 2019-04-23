9cf498948068409b12ae095dc08d0164650e0152
Luke Bakken <lukecbakken@gmail.com>
2019-04-23 13:36:18 -0600

    begin work on testing

--- spec/spec.js
+++ spec/spec.js
@@ -1,21 +1,73 @@
 var request = require('supertest');
-describe('loading express', function () {
-  var app;
+// var should = require('should');
+var app = require('../app');
+
+const good_cred = {
+  email: 'good@mail.com',
+  password: 'goodpass'
+}
+
+const bad_cred = {
+  email: 'bad@mail.com',
+  password: 'badpass'
+}
+
+const bad_pass = {
+  email: 'good@mail.com',
+  password: 'badpass'
+}
+
+describe('Index Routes', function() {
   beforeEach(function () {
-    app = require('../app');
-  });
-  afterEach(function () {
-    // app.close();
-    // setTimeout(done, 1000)
-  });
+  })
   it('responds to /', function testSlash(done) {
-  request(app)
-    .get('/')
-    .expect(200, done);
-  });
-  it('404 everything else', function testPath(done) {
     request(app)
-      .get('/foo/bar')
-      .expect(404, done);
+      .get('/')
+      .expect(200, done)
   });
-});
+  it('responds to /login', function testLogin(done) {
+    request(app)
+      .get('/login')
+      .expect(200, done)
+  });
+  it('should reject illegal login', function testIllegalLogin(done) {
+    request(app)
+      .post('/login')
+      .expect(200, done)
+  })
+  it('should accept legal login', function testLegalLogin(done) {
+    request(app)
+      .post('/login')
+      .expect(200, done)
+  })
+  it('responds to /register', function testRegister(done) {
+    request(app)
+      .get('/register')
+      .expect(200, done)
+  });
+  it('responds to /logout', function testSlash(done) {
+    request(app)
+      .get('/logout')
+      .expect(200, done)
+  });
+    it('404 everything else', function testPath(done) {
+      request(app)
+        .get('/foo/bar')
+        .expect(404, done);
+    });
+})
+
+describe('User Routes', function() {
+  beforeEach(function () {
+  })
+  it('responds to /', function testSlash(done) {
+    request(app)
+      .get('/')
+      .expect(200, done);
+    });
+    it('404 everything else', function testPath(done) {
+      request(app)
+        .get('/foo/bar')
+        .expect(404, done);
+    });
+})
