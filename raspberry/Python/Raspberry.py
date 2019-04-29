8a05a5220304d3a96d9d908aa01d1359a318c9eb
lbakken <lukecbakken@gmail.com>
2019-04-29 03:08:10 -0600

    updates to raspberry

--- raspberry/Python/Raspberry.py
+++ raspberry/Python/Raspberry.py
@@ -2,27 +2,31 @@ import serial;
 from picamera import PiCamera
 from time import sleep
 import datetime
-import psycogp2
+#import psycogp2
 
-connection = psycopg2.connect(user="sysadmin", password="pynative@#29", host="127.0.0.1", port="5432", database="postgres_db")
+#connection = psycopg2.connect(user="sysadmin", password="pynative@#29", host="127.0.0.1", port="5432", database="postgres_db")
 
 
 camera = PiCamera()
 
-arduino = serial.Serial('com1', 9600)
+arduino = serial.Serial('/dev/ttyUSB0', 9600)
 
 should_water = True
 
 def water_plant():
-	arduino.write('w')
+        arduino.write(b'w')
 
-def take_pic(now):
-	camera.capture('/home/pi/pics/' + now + '.jpg')
+sleep(5)
 
 while 1:
-	if(ser.in_waiting >0):
-		line = ser.readline()
-		print(line)
+        if should_water:
+                water_plant()
+        if(arduino.in_waiting >0):
+                line = arduino.readline()
+                print(line)
+                now = datetime.datetime.now()
+                #camera.capture('/home/pi/pics/' + now.strftime("%y-%m-%d-%H:%M:%S") + '.jpg')
+        sleep(3)
 
    # cursor = connection.cursor()
    # postgres_insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
@@ -30,10 +34,3 @@ while 1:
    # cursor.execute(postgres_insert_query, record_to_insert)
    # connection.commit()
 
-		now = datetime.datetime.now()
-		# camera.capture('/home/pi/pics/' + now + '.jpg')
-
-	if should_water:
-		water_plant()
-
-	sleep(3000)
