58bb2df27874d90fb8d8345568780361e8c1e828
lbakken <lukecbakken@gmail.com>
2019-04-29 03:55:40 -0600

    database connection begin

--- raspberry/Python/Raspberry.py
+++ raspberry/Python/Raspberry.py
@@ -2,14 +2,25 @@ import serial
 from picamera import PiCamera
 from time import sleep
 import datetime
-#import psycogp2
+import psycopg2
 
-#connection = psycopg2.connect(user="sysadmin", password="pynative@#29", host="127.0.0.1", port="5432", database="postgres_db")
+connection = psycopg2.connect(
+    user="sysadmin",
+    password="pynative@#29",
+    host="127.0.0.1",
+    port="5432",
+    database="postgres_db")
+
+   # cursor = connection.cursor()
+   # postgres_insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
+   # record_to_insert = ()
+   # cursor.execute(postgres_insert_query, record_to_insert)
+   # connection.commit()
 
 
 camera = PiCamera()
 
-arduino = serial.Serial('/dev/ttyUSB0', 9600)
+arduino = serial.Serial('/dev/ttyUSB1', 9600)
 
 should_water = True
 
@@ -21,25 +32,17 @@ sleep(5)
 day = datetime.date.today()
 water_today = False
 while 1:
-	now = datetime.datetime.now()
-	today = datetime.date.today()
-	if (day < today):
-		day = today
-		water_today = True
-	if (water_today and now.time() >= datetime.time(9,0,0) and now.time() <= datetime.time(10, 0, 0)):
-		water_today = False
-		water_plant()
-	if should_water:
-		water_plant()
-	if(arduino.in_waiting >0):
-		line = arduino.readline()
-		print(line)
-		#camera.capture('/home/pi/pics/' + now.strftime("%y-%m-%d-%H:%M:%S") + '.jpg')
-	sleep(3)
-
-   # cursor = connection.cursor()
-   # postgres_insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
-   # record_to_insert = ()
-   # cursor.execute(postgres_insert_query, record_to_insert)
-   # connection.commit()
+    now = datetime.datetime.now()
+    today = datetime.date.today()
+    if (day < today):
+        day = today
+        water_today = True
+    if (water_today and now.time() >= datetime.time(9,0,0) and now.time() <= datetime.time(10, 0, 0)):
+        water_today = False
+        water_plant()
+    if(arduino.in_waiting >0):
+        line = arduino.readline()
+        print(line)
+        #camera.capture('/home/pi/pics/' + now.strftime("%y-%m-%d-%H:%M:%S") + '.jpg')
+    sleep(6)
 
