54e42a51910927ef2399dd241af8494c7dad2aa7
Luke Bakken <lukecbakken@gmail.com>
2019-04-29 03:33:39 -0600

    auto water at 9am

--- raspberry/Python/Raspberry.py
+++ raspberry/Python/Raspberry.py
@@ -1,4 +1,4 @@
-import serial;
+import serial
 from picamera import PiCamera
 from time import sleep
 import datetime
@@ -14,19 +14,28 @@ arduino = serial.Serial('/dev/ttyUSB0', 9600)
 should_water = True
 
 def water_plant():
-        arduino.write(b'w')
+    arduino.write(b'w')
 
 sleep(5)
 
+day = datetime.date.today()
+water_today = False
 while 1:
-        if should_water:
-                water_plant()
-        if(arduino.in_waiting >0):
-                line = arduino.readline()
-                print(line)
-                now = datetime.datetime.now()
-                #camera.capture('/home/pi/pics/' + now.strftime("%y-%m-%d-%H:%M:%S") + '.jpg')
-        sleep(3)
+	now = datetime.datetime.now()
+	today = datetime.date.today()
+	if (day < today):
+		day = today
+		water_today = True
+	if (water_today and now.time() >= datetime.time(9,0,0) and now.time() <= datetime.time(10, 0, 0)):
+		water_today = False
+		water_plant()
+	if should_water:
+		water_plant()
+	if(arduino.in_waiting >0):
+		line = arduino.readline()
+		print(line)
+		#camera.capture('/home/pi/pics/' + now.strftime("%y-%m-%d-%H:%M:%S") + '.jpg')
+	sleep(3)
 
    # cursor = connection.cursor()
    # postgres_insert_query = """ INSERT INTO mobile (ID, MODEL, PRICE) VALUES (%s,%s,%s)"""
